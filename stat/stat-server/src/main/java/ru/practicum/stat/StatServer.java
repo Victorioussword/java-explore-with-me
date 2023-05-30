3c7c2add6b8578e9a03f6d1e01d209fd077d229a
Viktor <victorioussword@mail.ru>
2023-05-30 19:30:28 +0400

    19-29

--- stat/stat-server/src/main/java/ru/practicum/stat/StatServer.java
+++ stat/stat-server/src/main/java/ru/practicum/stat/StatServer.java
@@ -2,10 +2,12 @@ package ru.practicum.stat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class StatServer {
     public static void main(String[] args) {
+        System.setProperty("server.port", "9090");
         SpringApplication.run(StatServer.class, args);
     }
 }
--- stat/stat-server/src/main/java/ru/practicum/stat/service/StatService.java
+++ stat/stat-server/src/main/java/ru/practicum/stat/service/StatService.java
@@ -10,7 +10,6 @@ import ru.practicum.stat.model.ViewStat;
 import ru.practicum.stat.mapper.HitMapper;
 import ru.practicum.stat.repository.StatRepository;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -23,21 +22,24 @@ public class StatService {
     private final StatRepository statRepository;
 
     public OutputHitDto postHit(InputHitDto inputHitDto) {
-
+        log.info("StatService - postHit(). Создан {}", inputHitDto.toString());
         return HitMapper.toOutputHitDto(statRepository.save(HitMapper.toHit(inputHitDto)));
     }
 
     public List<ViewStat> getStat(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
 
         if (unique && uris != null) {
+            log.info("StatService - getStat(). Start {}, ENd {}, size {}, unique {}", start, end, uris.size(), unique);
             return statRepository.getStatUniqueIpUris(start, end, uris);  // уникальный IP, список ссылок есть
         } else if (unique && uris == null) {
+            log.info("StatService - getStat(). Start {}, ENd {}, size = 0, unique {}", start, end, unique);
             return statRepository.getStatUniqueIpNoUris(start, end);  // // уникальный IP, список ссылок нет
         } else if (!unique && uris != null) {
+            log.info("StatService - getStat(). Start {}, ENd {}, size {}, unique {}", start, end, uris.size(), unique);
             return statRepository.getStatUnUniqueIpUris(start, end, uris);  // не уникальный IP, список ссылок есть
         } else {
+            log.info("StatService - getStat(). Start {}, ENd {}, size = 0, unique {}", start, end, unique);
             return statRepository.getStatUnUniqueIpNoUris(start, end);  // не уникальный IP, список ссылок нет
         }
-
     }
 }
