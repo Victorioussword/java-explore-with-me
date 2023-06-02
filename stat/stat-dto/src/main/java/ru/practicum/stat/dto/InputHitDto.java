8fded65886c21fa714e3c28d2dfe6448676d7591
Viktor <victorioussword@mail.ru>
2023-06-02 19:44:37 +0400

    19-44

--- stat/stat-dto/src/main/java/ru/practicum/stat/dto/InputHitDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/stat/dto/InputHitDto.java
@@ -2,7 +2,6 @@ package ru.practicum.stat.dto;
 
 import lombok.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.experimental.FieldDefaults;
--- stat/stats-server/src/main/java/ru/practicum/stat/controller/StatController.java
+++ stat/stats-server/src/main/java/ru/practicum/stat/controller/StatController.java
@@ -3,7 +3,6 @@ package ru.practicum.stat.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stat.dto.InputHitDto;
 import ru.practicum.stat.dto.OutputHitDto;
